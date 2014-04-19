822ac85ed6ab9ab4dfff5bb3f280324297f59017
dgdavidgreene <dgdavidgreene@yahoo.com>
2014-04-18 18:10:44 -0700

    Initial

---
+++ ShoppingCart/ShoppingCart/Routing/DefaultRoute.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultRoute.cs" company="KriaSoft LLC">
+//   Copyright © 2013 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace ShoppingCart.Routing
+{
+    using System.Web.Routing;
+
+
+    public class DefaultRoute : Route
+    {
+        public DefaultRoute()
+            : base("{*path}", new DefaultRouteHandler())
+        {
+            this.RouteExistingFiles = false;
+        }
+    }
+}
